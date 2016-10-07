2118585ec591cca1fc86927ca898c37f2650a94b
John Hamm <johndhamm1@gmail.com>
2016-10-06 21:28:55 -0500

    server for heroku functional

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 
 const express = require('express')
 const app = express();
+const request = require('request'); //from vaughn's proxy
 
-const PORT = process.env.PORT || 6660
+const PORT = process.env.PORT || 3000
 app.set('port', PORT)
 
 // MIDDLEWARE (transform stream)
@@ -13,29 +14,49 @@ app.set('port', PORT)
 //   next();
 // });
 
-// WEATHERUNDERGROUND PROXY
-app.get('/t/*', (req, res) => {
-	console.log("req.body: ", req.body);
-  // let newURL = `${req.url}`.replace(/%22/g, '"');
-  // newURL = newURL.replace(/%20/g, ' ');
-  // factual.get(newURL, function (error, data) {
-  // res.send(data);
-  // });
+// app.use(express.static('public'));
 
+app.get('/*', (req, res) => {
+	console.log("req.url: ", req.url);
+	const coordinates = req.url;
+	const weatherCallURL = `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q${coordinates}.json`
+	request.get(weatherCallURL, (err, _, body) => {
+    console.log("body: ", body);
+    // res.send(body)
+  });
 })
+
+
+// WEATHERUNDERGROUND PROXY
+// app.get('/*', (req, res) => {
+// 	console.log("req.url: ", req.url);
+	//.get(`https://trailweatherproxy.herokuapp.com/${req.url}`)
+
+
+// GOOGLE MAPS PROXY (from Vaughn)
+// app.get('/maps/*', (req, res) => {
+//   let newMapsURL = req.url
+//   newMapsURL = `https://maps.googleapis.com${newMapsURL}`
+//   request.get(newMapsURL, (err, _, body) => {
+//     res.send(body)
+//   });
+// });
+
+
+// })
 
 app.listen(PORT, () => {
 	console.log(`Express server listening on port: ${PORT}`);
 });
 
 
-//ORIGINAL CALL FROM FACTORY
+// ORIGINAL CALL FROM FACTORY
 // function getCurrentWeather(lat, long){
-// 		return new Promise(function(resolve, reject){
-// 			$.ajax({
-// 				url: `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q/${lat},${long}.json`
-// 			}).done(function(data){
-// 				resolve(data);
-// 			});
+// 	return new Promise(function(resolve, reject){
+// 		$.ajax({
+// 			url: `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q/${lat},${long}.json`
+// 		}).done(function(data){
+// 			resolve(data);
 // 		});
-// 	}
+// 	});
+// }
