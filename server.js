87cd2b43333b49e35b3a7c00fe176afc3c2754ff
John Hamm <johndhamm1@gmail.com>
2016-10-06 21:49:56 -0500

    fix try with headers

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3000
 app.set('port', PORT)
 
 // MIDDLEWARE (transform stream)
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // app.use(express.static('public'));
 
