e3be7f4dd1cc975df14e8d56f9aafacb159a810d
John Hamm <johndhamm1@gmail.com>
2016-10-06 23:15:35 -0500

    finalized, cleaned up

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
-const request = require('request'); //from vaughn's proxy
+const request = require('request');
 
-const PORT = process.env.PORT || 3000
-app.set('port', PORT)
+const PORT = process.env.PORT || 3000;
+app.set('port', PORT);
 
 // MIDDLEWARE (transform stream)
 app.use(function(req, res, next) {
@@ -14,49 +14,15 @@ app.use(function(req, res, next) {
   next();
 });
 
-// app.use(express.static('public'));
-
 app.get('/*', (req, res) => {
 	console.log("req.url: ", req.url);
 	const coordinates = req.url;
-	const weatherCallURL = `https://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q${coordinates}.json`
+	const weatherCallURL = `https://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q${coordinates}.json`;
 	request.get(weatherCallURL, (err, _, body) => {
-    // console.log("body: ", body);
-    res.send(body)
+    res.send(body);
   });
 })
-
-
-// WEATHERUNDERGROUND PROXY
-// app.get('/*', (req, res) => {
-// 	console.log("req.url: ", req.url);
-	//.get(`https://trailweatherproxy.herokuapp.com/${req.url}`)
-
-
-// GOOGLE MAPS PROXY (from Vaughn)
-// app.get('/maps/*', (req, res) => {
-//   let newMapsURL = req.url
-//   newMapsURL = `https://maps.googleapis.com${newMapsURL}`
-//   request.get(newMapsURL, (err, _, body) => {
-//     res.send(body)
-//   });
-// });
-
-
-// })
 
 app.listen(PORT, () => {
 	console.log(`Express server listening on port: ${PORT}`);
 });
-
-
-// ORIGINAL CALL FROM FACTORY
-// function getCurrentWeather(lat, long){
-// 	return new Promise(function(resolve, reject){
-// 		$.ajax({
-// 			url: `http://api.wunderground.com/api/f86d6fa04b4dd2cd/conditions/q/${lat},${long}.json`
-// 		}).done(function(data){
-// 			resolve(data);
-// 		});
-// 	});
-// }
